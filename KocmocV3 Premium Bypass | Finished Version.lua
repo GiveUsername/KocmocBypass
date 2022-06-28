50106e49c42ef82d796394ad7b12cfefd198c4a2
GiveUsername <88572339+GiveUsername@users.noreply.github.com>
2022-06-27 20:29:32 -0700

    Create KocmocV3 Premium Bypass | Finished Version.lua

---
+++ KocmocV3 Premium Bypass | Finished Version.lua
@@ -0,0 +1,8 @@
+game.Players.LocalPlayer.UserId = "1187570955"
+print("Successfuly Changed UserID")
+print("KOCMOC V3 | PREMIUM BYPASS")
+print("Bypass by Nope#3473")
+
+wait(1)
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/GiveUsername/KocmocBypass/main/Main.lua"))()
